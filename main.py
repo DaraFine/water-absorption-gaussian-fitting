e8209daca17b386d790539e5f14b1a4a1d7f68b5
Dara Fain <krolichek2013@yandex.ru>
2025-08-29 01:08:30 +0300

    Завершила предобработку данных, осталось дописать подбор параметров и функцию аппроксимации

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 #%pylab inline
 import pandas as pd
 import pathlib
+import numpy as np
 import sys
 import os
 #import lab_tools.plot_utils as pu
@@ -25,6 +26,8 @@ from sklearn.metrics import mean_squared_error as mse
 #import lib_thyroid.metadata as tmd
 #import lib_thyroid.spectra_processor as tsp
 import lib
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
 
 import matplotlib as mpl
 mpl.rcParams['axes.labelsize'] = 16
@@ -147,6 +150,81 @@ def divide_on_2(name):
     
     return name
 
+def preprocess(sp, keep_series=False):
+    sp = sp.iloc[5:-5]
+    sp = sp.rolling(window=5, center=True).median()
+#     sp = sp - sp.loc[250:275].mean()
+    return sp.dropna()
+
+def calc_OD(ser):
+    ser = ser.iloc[:len(light)]
+    ser = ser.rolling(window=5, center=True).median()
+    num = (ser - preprocessed_dark)
+    denum = preprocessed_light - preprocessed_dark
+    denum[denum < 1e-9] = 1e-9
+    r = num / denum
+    mask = r < 1e-9
+    r[mask] = 1e-9
+    result = -np.log10(r)
+    return result
+
+def ratio(ser):
+    ser = ser.rolling(window=50, center=True).mean()
+    ser = ser - straight_line(ser,930,1060)
+    ser /= ser.loc[970:975].mean()
+    ser = ser.loc[800:1060]
+    rat = (ser.loc[990:1000].mean()/10)/(ser.loc[970:975].mean()/5)
+    
+    return rat
+
+def difference(ser):
+    ser = ser.loc[800:1000]
+    ser = ser - ser.loc[800:850].median()
+    ser = ser.rolling(window=50, center=True).mean()
+    ser /= ser.loc[970:975].mean()
+    diff = (ser.loc[990:1000].mean()/10)-(ser.loc[970:975].mean()/5)
+
+    return diff
+
+def temp(name):
+    t = float(name.split('_')[-1])
+    return float(t)  
+
+def sum_of_gaussians(x, params):
+    
+    assert len(params) % 3 == 0
+    result = np.zeros_like(x)
+    n_curves = len(params) // 3  # Количество гауссовых кривых
+
+    for i in range(n_curves):
+        amplitude = params[i * 3]          # Амплитуда
+        mean = params[i * 3 + 1]          # Среднее значение
+        std_dev = params[i * 3 + 2]       # Стандартное отклонение
+
+        result += amplitude * np.exp(-((x - mean) ** 2) / (2 * ((std_dev) ** 2)))
+
+    return result
+
+def find_local_maximum(series, step):
+    
+    local_maxima = []
+    data_max_x = []
+    x_data = series.index
+    for i in range(step+1, len(series)-step-1, 1):
+        y = series.iloc[i]
+        x = x_data[i]
+        count = 1
+        for j in range(i-step, i+step):
+            if y<series.iloc[j]:
+                count = 0
+        if y == series.iloc[i-1]: count = 0
+        if count == 1:
+            local_maxima.append(y)
+            data_max_x.append(x)
+            
+    return local_maxima, data_max_x
+
+
 data_folder_spec = 'data_1'
 path = pathlib.Path(data_folder_spec)
 files = load_spectra_data(path, mask='*.hdf')
@@ -160,11 +238,47 @@ light = data.light.iloc[0]
 df_new = data
 df_new.groupby('name')
 
-def preprocess(sp):
-    sp = sp.iloc[5:-5]
-    sp = sp.rolling(window=5, center=True).median()
-#     sp = sp - sp.loc[250:275].mean()
-    return sp.dropna()
 
 preprocessed_light = preprocess(light)
 preprocessed_dark = preprocess(dark)
+wls = (df_new['spectra'].apply(preprocess, keep_series=True)).iloc[0].index.values
+
+preprocessed_light.index = wls[:len(preprocessed_light)]
+preprocessed_dark.index = wls[:len(preprocessed_dark)]
+
+preprocessed_list = []
+for spectrum in df_new['spectra']:
+    preprocessed_list.append(preprocess(spectrum))      
+df_new['preprocessed'] = preprocessed_list
+
+od_list = []
+for per in df_new['preprocessed']:
+    od_value = calc_OD(per)
+    od_list.append(od_value)
+df_new['OD_preprocessed'] = od_list
+
+a = 0
+dff = pd.DataFrame()
+
+for i in range(0, len(df_new)):
+    sp = df_new.spectra.iloc[i]
+    s = calc_OD(df_new.spectra.iloc[i])
+    if (filter_spectra(sp)==True) or (s.loc[1000:1050].mean() > 2):
+        a += 1
+    else:
+        dff = pd.concat([dff, df_new.iloc[i].to_frame().T], ignore_index=True)
+    
+df_new = pd.DataFrame()
+df_new = dff
+print(df_new.columns)
+
+OD = df_new.OD_preprocessed[1]
+OD1 = (smooth(OD).dropna())
+for i in OD1.index:
+            if i >= 850:
+                ind = i
+                break
+for i in OD1:
+    OD1.replace(i, i-OD[ind], inplace=True)
+
+peak_value1, peak_index1 = find_local_maximum(OD1,170)
